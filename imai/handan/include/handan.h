300ebb3f5f7c7eb791b593077ad80e7bffb35b55
Yuki Imai <yussan0907@gmail.com>
2019-08-19 12:52:46 +0900

    [MOD]判断部修正

--- imai/handan/include/handan.h
+++ imai/handan/include/handan.h
@@ -7,15 +7,11 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2/convert.h>
-#include <gazebo_msgs/LinkStates.h>
-
+#include <geometry_msgs/Point.h>
+#include <std_msgs/Bool.h>
 /**
  * @class Handan
- * @brief Gazeboのメッセージを読み取り、Robot位置(tf"base_footprint")をpublishする。
+ * @brief 
  * @authors Yuki Imai(2019)
  * @copyright &copy; 2019,  Corporation
  */
@@ -25,18 +21,14 @@ public:
 	Handan();///<コンストラクタ
 	~Handan();///<デストラクタ
 protected:
-	ros::Subscriber sub_gazebo;///<gazebo_msgs入力ハンドラ
-	ros::Publisher pub_pose;///<ロボット位置Pose 発行ハンドラ
-	tf2_ros::TransformBroadcaster *tf2_brocast;///< ロボット位置tf発行ハンドラ
-	void callback_gazebo(const gazebo_msgs::LinkStates::ConstPtr &msg);///<gazebo_msgs入力コールバック
+	ros::Subscriber sub_enemy_pos;
 
-	/**
-	 * @brief from tf1 to tf2 、frame_id tfを求める。
-	 * @param[in] tf1 元tf
-	 * @param[in] tf2 先tf
-	 * @return tf1からtf2へ至るtf
-	 */
-	geometry_msgs::TransformStamped diff_geo_tf(const geometry_msgs::TransformStamped tf1 , const geometry_msgs::TransformStamped tf2);
-};
+	ros::Publisher pub_score_command;
+	ros::Publisher pub_enemyfollow_command;
+
+	void callback_enemy_pos(const geometry_msgs::Point::ConstPtr &msg);
+	geometry_msgs::Point last_enemy_pose_;
+	ros::Time last_enemy_time;
+};	
 
 #endif //HANDAN_H_
