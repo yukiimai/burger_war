aef21702ad8b33e02848831ffc063cd748626254
shimaldino <shimaldini@gmail.com>
2019-08-19 18:27:46 +0900

    try

--- shimada/run_three_points/include/run_three_points/run_three_points.h
+++ shimada/run_three_points/include/run_three_points/run_three_points.h
@@ -4,29 +4,43 @@
 #include <sstream>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Quaternion.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_broadcaster.h>
 
 class runThreePoints
 {
 private:
-  void callbackJudge();
+  void callbackJudge(const std_msgs::Bool::ConstPtr& msg);
   void callbackOdom(const nav_msgs::Odometry::ConstPtr& msg);
   void runStart();
-  void runStraight();
-  void runLeft();
-  void runRight();
+  void nextSeqence();
+  void calcDistOdom();
+  void calcAngleOdom();
+  //void runStraight(double distance);
+  //void turnLeft(double angle);
+  //void turnRight(double angle);
 
 private:
   double STRAIGHT_DIST = 1.0;
-  double LEFT_DIST = 0.3;
-  double RIGHT_DIST = 0.3;
-  double LEFT_ANGLE = 0.262;   //1.57/6(15deg)
-  double RIGHT_ANGLE = 0.262;  //1.57/6(15deg)
-  bool flag_run_straight = false;
-  bool flag_run_left = false;
-  bool flag_run_right = false;
+  double SIDE_DIST = 0.4;
+  double SIDE_ANGLE = 0.262;  //1.57/6(15deg)
   
+  enum NUM_SEQ{
+    NUM_SEQ_1,
+    NUM_SEQ_2,
+    NUM_SEQ_3,
+    NUM_SEQ_4,
+    NUM_SEQ_5,
+    NUM_SEQ_6,
+    NUM_SEQ_7,
+    NUM_SEQ_8,
+    NUM_SEQ_9,
+    NUM_SEQ_10,
+    NUM_SEQ_END
+  };
+
 public:
   runThreePoints();
   void spin();
@@ -40,4 +54,16 @@ public:
   ros::Subscriber sub_judge;
   ros::Subscriber sub_odom;
   ros::Publisher pub_run;  
+  
+  double odom_dist = 0.0;
+  double now_roll = 0.0;
+  double now_pitch = 0.0;
+  double now_yaw = 0.0;
+  double start_roll = 0.0;
+  double start_pitch = 0.0;
+  double start_yaw = 0.0;
+  double diff_rad = 0.0;  
+  
+  NUM_SEQ num_seq;
 };
+
--- shimada/run_three_points/src/run_three_points.cpp
+++ shimada/run_three_points/src/run_three_points.cpp
@@ -4,25 +4,27 @@ runThreePoints::runThreePoints()
 {
   ros::NodeHandle nh;
   
+  sub_judge = nh.subscribe("scoreget_activate", 5, &runThreePoints::callbackJudge, this);
   sub_odom = nh.subscribe("odom", 5, &runThreePoints::callbackOdom, this);
-
-  pub_run = nh.advertise<geometry_msgs::Twist>("cmd_vel", 5);
+  pub_run = nh.advertise<geometry_msgs::Twist>("score_vel", 5);
+  
+  num_seq = NUM_SEQ_1;
 }
 
-void runThreePoints::callbackJudge()
+void runThreePoints::callbackJudge(const std_msgs::Bool::ConstPtr& msg)
 {
-  if (sub_judge)
+  static bool pre_msg = false;
+
+  if (!pre_msg && msg->data) //走行開始時の初期化
   {
-    start_position_odom = now_odom;//
-    flag_run_straight = true;
-    flag_run_left = true;
-    flag_run_right = true;
-  }else 
+    start_position_odom = now_odom;//現在位置を走行開始地点とする
+    num_seq = NUM_SEQ_1;
+  }else if(!msg->data)//終了
   {
-    flag_run_straight = false;
-    flag_run_left = false;
-    flag_run_right = false;
+    num_seq = NUM_SEQ_END;
   }
+    
+  pre_msg = msg->data;
 }
 
 void runThreePoints::callbackOdom(const nav_msgs::Odometry::ConstPtr& msg)
@@ -34,215 +36,169 @@ void runThreePoints::callbackOdom(const nav_msgs::Odometry::ConstPtr& msg)
   quat_msg.w = now_odom.pose.pose.orientation.w;
 }
 
-void runThreePoints::runStart()
+void runThreePoints::calcDistOdom()
 {
-  twist_run.linear.x = 1.0;
+  static float diff_x;
+  static float diff_y;
+  
+  diff_x = now_odom.pose.pose.position.x - start_position_odom.pose.pose.position.x;
+  diff_y = now_odom.pose.pose.position.y - start_position_odom.pose.pose.position.y;
+  odom_dist = sqrt(pow(diff_x, 2) + pow(diff_y, 2));
+}
+
+void runThreePoints::calcAngleOdom()
+{
+  tf::Quaternion quat(quat_msg.x, quat_msg.y, quat_msg.z, quat_msg.w);
+  tf::Matrix3x3(quat).getRPY(now_roll, now_pitch, now_yaw);//クォータニオン→オイラー角
+  
+  tf::Quaternion start_quat(start_position_odom.pose.pose.orientation.x,
+                            start_position_odom.pose.pose.orientation.y,
+                            start_position_odom.pose.pose.orientation.z,
+                            start_position_odom.pose.pose.orientation.w);
+  tf::Matrix3x3(start_quat).getRPY(start_roll, start_pitch, start_yaw);//クォータニオン→オイラー角
+
+  diff_rad = start_yaw - now_yaw;
+}
+
+void runThreePoints::nextSeqence()
+{
+  twist_run.linear.x = 0.0;
   twist_run.angular.z = 0.0;
-  
+  start_position_odom = now_odom;
+  //num_seq++;
 }
 
-void runThreePoints::runStraight()
+void runThreePoints::runStart()//走行シナリオ
 {
-  static float diff_x;
-  static float diff_y;
-  static float odom_dist;
-  bool flag_straight_first = false;
-  bool flag_straight_second = false;  
-  
-  while (flag_run_straight)
+  switch(num_seq)
   {
-    diff_x = start_position_odom.pose.pose.position.x - now_odom.pose.pose.position.x;
-    diff_y = start_position_odom.pose.pose.position.y - now_odom.pose.pose.position.y;
-    odom_dist = sqrt(pow(diff_x, 2) + pow(diff_y, 2));
-    
-    if (odom_dist < STRAIGHT_DIST && !flag_straight_first)
-    {
-      twist_run.linear.x = 1.0;
-      twist_run.angular.z = 0.0;
-    } else if (odom_dist > STRAIGHT_DIST && !flag_straight_first)
-    {
-      flag_straight_first = true;
-    }
-
-    if (odom_dist < STRAIGHT_DIST && !flag_straight_second && flag_straight_first)
-    {
-      twist_run.linear.x = -1.0;
-      twist_run.angular.z = 0.0;
-    } else if (odom_dist <= 0.0 && !flag_straight_second && flag_straight_first)
-    {
-      flag_straight_second = true;
-    }
-    
-    if (flag_straight_first && flag_straight_second)
-    {
-      flag_run_straight = false;
-      flag_straight_first = false;
-      flag_straight_second = false;
+    case NUM_SEQ_1://前進
+      calcDistOdom();
+      if(odom_dist >= STRAIGHT_DIST)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_2;
+      }else 
+      {
+        twist_run.linear.x = 1.0;
+        twist_run.angular.z = 0.0;
+      }  
+      break;
+    case NUM_SEQ_2://後退
+      calcDistOdom();
+      if(odom_dist >= STRAIGHT_DIST)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_3;
+      }else 
+      {  
+        twist_run.linear.x = -1.0;
+        twist_run.angular.z = 0.0;
+      }
+      break;
+    case NUM_SEQ_3://左回転
+      calcAngleOdom();
+      if(diff_rad >= SIDE_ANGLE)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_4;
+      }else 
+      {  
+        twist_run.linear.x = 0.0;
+        twist_run.angular.z = 0.1;
+      }
+      break;
+    case NUM_SEQ_4://前進
+      calcDistOdom();
+      if(odom_dist >= SIDE_DIST)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_5;
+      }else 
+      {  
+        twist_run.linear.x = 1.0;
+        twist_run.angular.z = 0.0;
+      }
+      break;
+    case NUM_SEQ_5://後退
+      calcDistOdom();
+      if(odom_dist >= SIDE_DIST)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_6;
+      }else 
+      {  
+        twist_run.linear.x = -1.0;
+        twist_run.angular.z = 0.0;
+      }
+      break;
+    case NUM_SEQ_6://右回転
+      calcAngleOdom();
+      if(diff_rad <= SIDE_ANGLE)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_7;
+      }else 
+      {  
+        twist_run.linear.x = 0.0;
+        twist_run.angular.z = -0.1;
+      }
+      break;
+    case NUM_SEQ_7://右回転
+      calcAngleOdom();
+      if(diff_rad <= SIDE_ANGLE)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_8;
+      }else 
+      {  
+        twist_run.linear.x = 0.0;
+        twist_run.angular.z = -0.1;
+      }
+      break;
+    case NUM_SEQ_8://前進
+      calcDistOdom();
+      if(odom_dist >= SIDE_DIST)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_9;
+      }else 
+      {  
+        twist_run.linear.x = 1.0;
+        twist_run.angular.z = 0.0;
+      }
+      break;
+    case NUM_SEQ_9://後退
+      calcDistOdom();
+      if(odom_dist >= SIDE_DIST)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_10;
+      }else 
+      {  
+        twist_run.linear.x = -1.0;
+        twist_run.angular.z = 0.0;
+      }
+      break;
+    case NUM_SEQ_10://左回転
+      calcAngleOdom();
+      if(diff_rad >= SIDE_ANGLE)
+      {
+        nextSeqence();
+        num_seq = NUM_SEQ_END;
+      }else 
+      {  
+        twist_run.linear.x = 0.0;
+        twist_run.angular.z = 0.1;
+      }
+      break;                  
+    case NUM_SEQ_END:
       twist_run.linear.x = 0.0;
       twist_run.angular.z = 0.0;
-    }
-    pub_run.publish(twist_run);
-  }
-}
-
-void runThreePoints::runLeft()
-{
-  static float diff_x;
-  static float diff_y;
-  static float diff_omega;
-  static float odom_dist;
-  bool flag_left_first = false;
-  bool flag_left_second = false;
-  bool flag_left_third = false;
-  bool flag_left_fourth = false;
-  
-  double now_r, now_p, now_y;//出力値
-  double start_r, start_p, start_y;//出力値
-
-  tf::Quaternion quat(quat_msg.x, quat_msg.y, quat_msg.z, quat_msg.w);
-  tf::Matrix3x3(quat).getRPY(now_r, now_p, now_y);//クォータニオン→オイラー角
-  
-  tf::Quaternion start_quat(start_position_odom.pose.pose.orientation.x,
-                            start_position_odom.pose.pose.orientation.y,
-                            start_position_odom.pose.pose.orientation.z,
-                            start_position_odom.pose.pose.orientation.w);
-  tf::Matrix3x3(start_quat).getRPY(start_r, start_p, start_y);//クォータニオン→オイラー角
-  
-  while (flag_run_left)
-  {
-    diff_x = start_position_odom.pose.pose.position.x - now_odom.pose.pose.position.x;
-    diff_y = start_position_odom.pose.pose.position.y - now_odom.pose.pose.position.y;
-    odom_dist = sqrt(pow(diff_x, 2) + pow(diff_y, 2));
-    diff_omega = start_y - now_y;
-    
-    if (now_y < diff_omega && !flag_left_first)
-    {
-      twist_run.linear.x = 0.0;
-      twist_run.angular.z = 0.1;
-    } else if (now_y >= diff_omega && !flag_left_first)
-    {
-      flag_left_first = true;
-    }
-
-    if (odom_dist < LEFT_DIST && !flag_left_second && flag_left_first)
-    {
-      twist_run.linear.x = 1.0;
-      twist_run.angular.z = 0.0;
-    } else if (odom_dist >= LEFT_DIST && !flag_left_second && flag_left_first)
-    {
-      flag_left_second = true;
-    }
-
-    if (odom_dist < LEFT_DIST && !flag_left_third && flag_left_second)
-    {
-      twist_run.linear.x = -1.0;
-      twist_run.angular.z = 0.0;
-    } else if (odom_dist <= 0.0 && !flag_left_third && flag_left_second)
-    {
-      flag_left_third = true;
-    }    
-
-    if (now_y < diff_omega && !flag_left_fourth && flag_left_third)
-    {
-      twist_run.linear.x = 0.0;
-      twist_run.angular.z = -0.1;
-    } else if (now_y <= 0.0 && !flag_left_fourth && flag_left_third)
-    {
-      flag_left_fourth = true;
-    }
-    
-    if (flag_left_first && flag_left_second && flag_left_third && flag_left_fourth)
-    {
-      flag_run_left = false;
-      flag_left_first = false;
-      flag_left_second = false;
-      flag_left_third = false;
-      flag_left_fourth = false;
-      twist_run.linear.x = 0.0;
-      twist_run.angular.z = 0.0;
-    }
-    pub_run.publish(twist_run);
-  }
-}
-
-void runThreePoints::runRight()
-{
-  static float diff_x;
-  static float diff_y;
-  static float diff_omega;
-  static float odom_dist;
-  bool flag_right_first = false;
-  bool flag_right_second = false;
-  bool flag_right_third = false;
-  bool flag_right_fourth = false;
-  
-  double now_r, now_p, now_y;//出力値
-  double start_r, start_p, start_y;//出力値
-
-  tf::Quaternion quat(quat_msg.x, quat_msg.y, quat_msg.z, quat_msg.w);
-  tf::Matrix3x3(quat).getRPY(now_r, now_p, now_y);//クォータニオン→オイラー角
-  
-  tf::Quaternion start_quat(start_position_odom.pose.pose.orientation.x,
-                            start_position_odom.pose.pose.orientation.y,
-                            start_position_odom.pose.pose.orientation.z,
-                            start_position_odom.pose.pose.orientation.w);
-  tf::Matrix3x3(start_quat).getRPY(start_r, start_p, start_y);//クォータニオン→オイラー角
-  
-  while (flag_run_right)
-  {
-    diff_x = start_position_odom.pose.pose.position.x - now_odom.pose.pose.position.x;
-    diff_y = start_position_odom.pose.pose.position.y - now_odom.pose.pose.position.y;
-    odom_dist = sqrt(pow(diff_x, 2) + pow(diff_y, 2));
-    diff_omega = start_y - now_y;
-    
-    if (diff_omega < RIGHT_ANGLE && !flag_right_first)
-    {
-      twist_run.linear.x = 0.0;
-      twist_run.angular.z = -0.1;
-    } else if (diff_omega >= RIGHT_ANGLE && !flag_right_first)
-    {
-      flag_right_first = true;
-    }
-
-    if (odom_dist < RIGHT_DIST && !flag_right_second && flag_right_first)
-    {
-      twist_run.linear.x = 1.0;
-      twist_run.angular.z = 0.0;
-    } else if (odom_dist >= RIGHT_DIST && !flag_right_second && flag_right_first)
-    {
-      flag_right_second = true;
-    }
-
-    if (odom_dist < RIGHT_DIST && !flag_right_third && flag_right_second)
-    {
-      twist_run.linear.x = -1.0;
-      twist_run.angular.z = 0.0;
-    } else if (odom_dist <= 0.0 && !flag_right_third && flag_right_second)
-    {
-      flag_right_third = true;
-    }    
-
-    if (diff_omega > RIGHT_ANGLE && !flag_right_fourth && flag_right_third)
-    {
-      twist_run.linear.x = 0.0;
-      twist_run.angular.z = 0.1;
-    } else if (diff_omega <= 0.0 && !flag_right_fourth && flag_right_third)
-    {
-      flag_right_fourth = true;
-    }
-    
-    if (flag_right_first && flag_right_second && flag_right_third && flag_right_fourth)
-    {
-      flag_run_right = false;
-      flag_right_first = false;
-      flag_right_second = false;
-      flag_right_third = false;
-      flag_right_fourth = false;
-      twist_run.linear.x = 0.0;
-      twist_run.angular.z = 0.0;
-    }
-    pub_run.publish(twist_run);
-  }
+      break; 
+    default:
+      break;
+  }  
 }
 
 void runThreePoints::spin()
@@ -251,7 +207,10 @@ void runThreePoints::spin()
   while(ros::ok())
   {
     //上位から指令が来たら走り始める
-    //runStart();
+    runStart();
+
+    pub_run.publish(twist_run);    
+
     ros::spinOnce();
     rate.sleep();
   }
